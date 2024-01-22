f091764ce3cbc3c089b5a12a50800ba106fb1a62
Gabe Ochoa <gabeochoa@meta.com>
2024-01-21 20:19:27 -0500

    cards with two columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { createContext, useCallback, useContext, useState } from 'react';
 
 const Link = (props: JSX.IntrinsicElements['a']) => (
   <a
@@ -7,81 +7,211 @@ const Link = (props: JSX.IntrinsicElements['a']) => (
   />
 );
 
+interface ITray {
+  cards: ICard[];
+  id: number;
+  label: string;
+}
+
 interface ICard {
   id: number;
   label: string;
 }
 
-export default function App() {
-  const [items, setItems] = useState<ICard[]>([
-    { id: 0, label: 'item 1' },
-    { id: 1, label: 'item 2' },
-    { id: 2, label: 'item 3' },
-  ]);
-  const [draggedList, setDraggedList] = useState<ICard[]>([]);
-  const [isDragging, setIsDragging] = useState<boolean>(false);
+interface ITrayContext {
+  moveCard: (
+    tray_one: ITray['id'],
+    tray_two: ITray['id'],
+    card_id: ICard['id'],
+  ) => void;
+  setTrays: (trays: ITray[]) => void;
+  trays: ITray[];
+}
 
-  const target = `mt-4 p-4 bg-white rounded-lg shadow-lg border-dashed border-2 min-h-60 ${isDragging ? 'border-black' : 'border-indigo-300'}`;
+const TrayContext = createContext<ITrayContext>({
+  moveCard: () => {},
+  setTrays: () => {},
+  trays: [],
+});
+
+function TrayContextProvider({ children }) {
+  const [trays, setTrays] = useState<ITray[]>([
+    {
+      id: 0,
+      label: 'new arrivals',
+      // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+      cards: [
+        { id: 0, label: 'item 1' },
+        { id: 1, label: 'item 2' },
+      ],
+    },
+    {
+      id: 1,
+      label: 'doctor1',
+      // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+      cards: [{ id: 2, label: 'item 3' }],
+    },
+    {
+      id: 2,
+      label: 'doctor2',
+      // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+      cards: [{ id: 3, label: 'item 4' }],
+    },
+  ]);
+
+  const moveCard = useCallback(
+    (from_tray: number, to_tray: number, card_id: number) => {
+      setTrays((prevTrays) => {
+        const newTrays = [...prevTrays];
+        const fromTray = prevTrays.find((tray) => tray.id === from_tray);
+        const toTray = prevTrays.find((tray) => tray.id === to_tray);
+        if (fromTray && toTray) {
+          const cardToMove = fromTray.cards.find((card) => card.id === card_id);
+          if (cardToMove) {
+            // Remove the card from the from tray
+            fromTray.cards = fromTray.cards.filter(
+              (card) => card.id !== card_id,
+            );
+            // Add the card to the to tray
+            toTray.cards.push(cardToMove);
+          } else {
+            console.log('didnt find card');
+          }
+        }
+        return newTrays;
+      });
+    },
+    [],
+  );
+
+  return (
+    <TrayContext.Provider
+      value={{
+        moveCard,
+        setTrays,
+        trays,
+      }}
+    >
+      {children}
+    </TrayContext.Provider>
+  );
+}
+
+function Tray({
+  cards,
+  horizontal,
+  id,
+  name,
+}: {
+  cards: ICard[];
+  horizontal: boolean;
+  id: string;
+  name: string;
+}) {
+  const { moveCard } = useContext(TrayContext);
+  const size = 100;
+  const MAX_CARDS = 3;
+
+  return (
+    <div
+      className="mt-5 rounded-lg p-4 shadow-lg"
+      onDragOver={(event) => {
+        event.preventDefault();
+      }}
+      onDrop={(event) => {
+        event.preventDefault();
+        if (cards.length == MAX_CARDS) {
+          return;
+        }
+
+        const data = JSON.parse(event.dataTransfer.getData('data'));
+        const { card_id, tray_id } = data;
+        moveCard(Number(tray_id), Number(id), Number(card_id));
+      }}
+    >
+      <div className="mb-2 text-xl font-bold">{name}</div>
+      <ul
+        className={`m-0 list-none border border-indigo-600 bg-indigo-100 p-0 px-1 ${horizontal ? 'flex flex-wrap' : ''}`}
+        style={{
+          height: !horizontal ? size * 1.1 * MAX_CARDS : 110,
+          width: horizontal ? size * 1.1 * MAX_CARDS : 110,
+        }}
+      >
+        {cards.map((item: ICard) => {
+          return (
+            <li
+              className="mb-2 cursor-move border border-indigo-300 bg-white p-4"
+              draggable={true}
+              id={`${item.id}`}
+              key={item.id}
+              onDragStart={(event) => {
+                const data = {
+                  card_id: event.currentTarget.id,
+                  tray_id: id,
+                };
+                event.dataTransfer.setData('data', JSON.stringify(data));
+              }}
+              style={{
+                height: size,
+                width: size,
+              }}
+            >
+              {item.label}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Main() {
+  const { trays } = useContext(TrayContext);
 
   return (
     <div className="mx-auto my-8 mt-10 w-8/12 rounded border border-gray-200 p-4 shadow-md dark:border-neutral-600 dark:bg-neutral-800 dark:shadow-none">
       <h1 className="mb-4 text-4xl">Welcome</h1>
-      <div className="grid grid-cols-2 gap-1">
-        <div className="mt-5 rounded-lg bg-white p-4 shadow-lg">
-          <ul className="m-0 min-h-40 list-none border border-indigo-600 bg-indigo-300 p-0">
-            {items.map((item) => {
-              return (
-                <li
-                  className="mb-2 cursor-move border border-indigo-300 bg-white p-4"
-                  draggable={true}
-                  id={`${item.id}`}
-                  key={item.id}
-                  onDragStart={(event) => {
-                    event.dataTransfer.setData('id', event.currentTarget.id);
-                  }}
-                >
-                  {item.label}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div
-          className={target}
-          onDragOver={(event) => {
-            event.preventDefault();
-            setIsDragging(true);
-          }}
-          onDrop={(event) => {
-            event.preventDefault();
-            const id = event.dataTransfer.getData('id');
-            const item = items.find((x) => x.id == Number(id));
-            if (item) {
-              setDraggedList([...draggedList, item]);
-              setIsDragging(false);
-              setItems(
-                items.filter((el) => {
-                  return el.id != item.id;
-                }),
-              );
+      <div className="columns-2 gap-8">
+        <div className="break-after-column">
+          {trays.map((tray: ITray, index: number) => {
+            if (index != 0) {
+              return null;
             }
-          }}
-        >
-          <ul>
-            {draggedList.map((item) => {
-              return (
-                <li
-                  className="mb-2 cursor-move border border-indigo-300 bg-white p-4"
-                  id={`{item.id}`}
-                  key={item.id}
-                >
-                  {item.label}
-                </li>
-              );
-            })}
-          </ul>
+            return (
+              <Tray
+                cards={tray.cards}
+                horizontal={false}
+                id={String(tray.id)}
+                key={tray.id}
+                name={tray.label}
+              />
+            );
+          })}
+        </div>
+        <div className="">
+          {trays.map((tray: ITray, index: number) => {
+            if (index == 0) {
+              return null;
+            }
+            return (
+              <Tray
+                cards={tray.cards}
+                horizontal={true}
+                id={String(tray.id)}
+                key={tray.id}
+                name={tray.label}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
+  );
+}
+export default function App() {
+  return (
+    <TrayContextProvider>
+      <Main />
+    </TrayContextProvider>
   );
 }
