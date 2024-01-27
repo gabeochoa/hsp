61e049cc8fa2757b52bdc66ffda7586f93abb0c8
Gabe Ochoa <gabeochoa@meta.com>
2024-01-26 22:42:59 -0500

    impact - add dead icon for dead cards

--- src/Tray.tsx
+++ src/Tray.tsx
@@ -12,6 +12,58 @@ import {
 
 const CARD_SIZE = 150;
 
+function DeadIcon() {
+  return (
+    <div
+      style={{
+        marginTop: -CARD_SIZE,
+        paddingRight: 75,
+      }}
+    >
+      <svg
+        className="h-24 w-24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
+
+function LockIcon() {
+  return (
+    <div
+      style={{
+        marginTop: -CARD_SIZE,
+        paddingRight: 75,
+      }}
+    >
+      <svg
+        className="h-24 w-24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={1.5}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
+
 function CardPlaceholder() {
   return (
     <li
@@ -81,7 +133,6 @@ function Card({ item, tray_id }: { item: Entity; tray_id: number }) {
       </div>
     );
   };
-  const draggable = !item.get<HasAffliction>('HasAffliction').locked();
 
   const background_color = () => {
     const health = item.get<HasHealth>('HasHealth').health;
@@ -99,6 +150,10 @@ function Card({ item, tray_id }: { item: Entity; tray_id: number }) {
     }
     return 'bg-white';
   };
+
+  const locked = item.get<HasAffliction>('HasAffliction').locked();
+  const dead = item.has('IsDead');
+  const draggable = !locked;
 
   return (
     <li
@@ -118,29 +173,8 @@ function Card({ item, tray_id }: { item: Entity; tray_id: number }) {
       }}
     >
       <CardInner />
-      {!draggable && (
-        <div
-          style={{
-            marginTop: -CARD_SIZE,
-            paddingRight: 75,
-          }}
-        >
-          <svg
-            className="h-24 w-24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={1.5}
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
-      )}
+      {locked && <LockIcon />}
+      {dead && <DeadIcon />}
     </li>
   );
 }
